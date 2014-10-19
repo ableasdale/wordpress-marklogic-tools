64469873b454f5f86dce9f006d5a0f29691aa703
Alex Bleasdale <ableasdale@marklogic.com>
2014-10-19 10:24:46 +0100

    Added basic maven structure and IntelliJ project file ready for writing tests

---
+++ src/main/java/com/xmlmachines/Main.java
@@ -0,0 +1,21 @@
+package com.xmlmachines;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexb
+ * Date: 19/10/14
+ * Time: 10:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    private static Logger LOG = LoggerFactory.getLogger(Main.class.getCanonicalName());
+
+
+    public static void main(String[] args) {
+        LOG.info("Ok..");
+    }
+}
