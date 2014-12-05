9bac62d57ccc47b5c40e7fc8014e3fd40827e85f
Alex Bleasdale <ableasdale@marklogic.com>
2014-12-05 16:22:00 +0000

    Not sure whether this test breaks (all work on the flight now committed - test later)

---
+++ src/test/java/TestPublicationWorkflow.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexb
+ * Date: 01/12/14
+ * Time: 19:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestPublicationWorkflow {
+
+    // Can I use RestAssured to Create a Quick Draft, submit it and then check to see whether it's been saved?
+
+
+    @Test
+    public void TestPostsResource() {
+        given().when().auth().basic("q", "q").
+                get("http://localhost:8003/wp-admin/posts.xqy").
+                then().log().body().statusCode(200).body().parser().;
+    }
+}
